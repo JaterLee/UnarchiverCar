714f3394aa563bb87f36fd19ad8d42021fca4e0d
Jater <diaozhatian@666.com>
2018-11-21 00:23:11 +0800

    ljz_实现拖拽

--- UnarchiverCar/JAInputView.swift
+++ UnarchiverCar/JAInputView.swift
@@ -9,17 +9,45 @@
 import Cocoa
 
 class JAInputView: NSView {
-
+    private let titleLabel : NSTextField = NSTextField.init()
+    
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
     }
     
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
-        self.registerForDraggedTypes([NSPasteboard])
     }
     
     required init?(coder decoder: NSCoder) {
         super.init(coder: decoder)
+        self.wantsLayer = true
+        self.layer?.backgroundColor = NSColor.white.cgColor
+        self.registerForDraggedTypes([.fileURL])
+        
+        self.titleLabel.stringValue = "请拖入解压资源文件"
+        self.titleLabel.alignment = NSTextAlignment.center
+        self.titleLabel.textColor = NSColor.darkGray
+        self.titleLabel.frame = NSMakeRect(0, 0, NSWidth(self.frame), NSHeight(self.frame));
+        self.addSubview(self.titleLabel)
+        
+    }
+    
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        let pboard = sender.draggingPasteboard
+        if pboard.types?.contains(.fileURL) ?? false {
+            return NSDragOperation.copy
+        }
+        return NSDragOperation.link
+
+    }
+    
+    override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        let zPasteboard = sender.draggingPasteboard
+        let list = zPasteboard.propertyList(forType: .fileURL)
+        let file = URL.init(string: list as! String)
+        self.titleLabel.stringValue = file?.absoluteString ?? ""
+        return true
     }
 }
+
